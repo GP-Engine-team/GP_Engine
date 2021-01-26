bfd32d96155e271be3a21a2cb598a0f468994ec9
Jonathan <j.six@student.isartdigital.com>
2021-01-26 16:38:56 +0100

    Move code in inl

--- include/Engine/Core/Tools/Format.hpp
+++ include/Engine/Core/Tools/Format.hpp
@@ -22,17 +22,8 @@ namespace Engine::Core::Tools
      * @return std::string 
      */
     template<typename ... Args>
-    std::string stringformat( const std::string& format, Args ... args )
-    {
-        int size = snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
-        
-        assert(size > 0, "Error during formatting.");
-
-        std::unique_ptr<char[]> buf( new char[ size ] ); 
-        snprintf( buf.get(), size, format.c_str(), args ... );
-        
-        return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
-    }
-    
+    [[nodiscard]]
+    std::string stringformat(const std::string& format, Args ... args) noexcept;
 } /*namespace Engine::Core::Tools*/
 
+#include "Format.inl"
