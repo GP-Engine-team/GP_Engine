ada928a15e18b663d4a314b78837e33a0b1d61cb
Jonathan <j.six@student.isartdigital.com>
2021-01-28 16:35:59 +0100

    Rework main example and add windows demo (without time)

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
+#include "Engine/Core/Rendering/Renderer/RendererGLFW_GL46.hpp"
+
 #include "Engine/Core/Debug/Assert.hpp"
 #include "Engine/Core/Debug/Log.hpp"
 
+using namespace Engine::Core::Renderering;
 using namespace Engine::Core::Debug;
 
-int main()
+void logExample()
 {
-    // Log::setSetting(ESetting::ALWAYS_PRINT_LOG_FILE, true);
-
     Log::logFileHeader();
 
     Log::logInitializationStart("logInitializationStart");
@@ -24,9 +25,19 @@ int main()
     FUNCT_ERROR("functError");
     FUNCT_WARNING("functWarning");
 
-    GPE_ASSERT(false, "GPE_assertInfo");
+    GPE_ASSERT(true, "GPE_assertInfo");
+}
+
+int main()
+{
+    // Log::setSetting(ESetting::ALWAYS_PRINT_LOG_FILE, true);
+
+    RendererGLFW_GL46 ren (WindowCreateArg{"GP engine", 600, 900});
+
+    logExample();
 
     Log::closeAndTryToCreateFile();
 
     return 0;
 }
+
