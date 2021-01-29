deb4188cf67f0a6329a8580f724a185548160457
Jonathan <j.six@student.isartdigital.com>
2021-01-29 11:46:58 +0100

    Add GPU foptimization flag

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "Engine/Core/Rendering/Window/WindowGLFW.hpp"
 #include "Engine/Core/Rendering/Renderer/RendererGLFW_GL46.hpp"
 
 #include "Engine/Core/Debug/Assert.hpp"
@@ -7,6 +8,12 @@
 
 using namespace Engine::Core::Renderering;
 using namespace Engine::Core::Debug;
+
+extern "C"
+{
+    _declspec(dllexport) int NvOptimusEnablement = 1;
+    _declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
 
 void logExample()
 {
@@ -32,7 +39,8 @@ int main()
 {
     // Log::setSetting(ESetting::ALWAYS_PRINT_LOG_FILE, true);
 
-    RendererGLFW_GL46 ren (WindowCreateArg{"GP engine", 600, 900});
+    WindowGLFW win(WindowCreateArg{"GP engine", 600, 900});
+    RendererGLFW_GL46 ren (win);
 
     logExample();
 
