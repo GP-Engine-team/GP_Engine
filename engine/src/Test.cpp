759aeb557dbc26d5b104d67628333f48a0b83f54
Thomas DALLARD <t.dallard@debian>
2021-02-01 15:29:01 +0100

    HotReload : - Projects are setup correctly. - An example has been added in main.cpp and exampleReloadedCode.cpp. - To run, ctrl+f5. - You can modify the inside of myTestFunction(). - To build at run time, ctrl shift + b, the dll will be reloaded.

---
+++ engine/src/Test.cpp
@@ -0,0 +1,8 @@
+#include "Test.h"
+
+#include <iostream>
+
+void Test::foo()
+{
+    std::cout << "YEAH" << std::endl;
+}
---
+++ projects/GPGame/exampleReloadedCode.cpp
@@ -0,0 +1,10 @@
+#include "exampleReloadedCode.hpp"
+
+#include <cstdio>
+
+#include "Engine/Core/Debug/Log.hpp"
+
+void myTestFunction()
+{
+	Engine::Core::Debug::Log::log("code reloaded !");
+}
