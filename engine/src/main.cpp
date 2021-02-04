bb9d45c6326e8de4774928a9df5df023958c7341
Jonathan <j.six@student.isartdigital.com>
2021-02-04 15:03:58 +0100

    Add link into driver static var in main

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -9,11 +9,17 @@
 using namespace Engine::Core::Renderering;
 using namespace Engine::Core::Debug;
 
+#ifdef _WIN32
+// Use discrete GPU by default.
 extern "C"
 {
-    _declspec(dllexport) int NvOptimusEnablement = 1;
-    _declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+    // http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
+    __declspec(dllexport) int NvOptimusEnablement = 1;
+
+    // https://community.amd.com/t5/firepro-development/can-an-opengl-app-default-to-the-discrete-gpu-on-an-enduro/m-p/279441#M471
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
+#endif
 
 void logExample()
 {
