569ed236d8cd519a212b0602649a648e2d80b348
Jonathan <j.six@student.isartdigital.com>
2021-02-08 13:17:02 +0100

    Rework resource manager and create example in main.cpp

--- engine/include/Engine/Resources/ResourcesManager.hpp
+++ engine/include/Engine/Resources/ResourcesManager.hpp
@@ -6,166 +6,133 @@
 
 #pragma once
 
-#include <unordered_map>
-#include <string>
-#include <utility>
-#include <typeinfo>
 #include "Engine/Core/Debug/Log.hpp"
+#include <string>
+#include <typeinfo>
+#include <unordered_map>
+#include <utility>
 
 namespace Engine::Resources
 {
-    //Technical inspired about understanding example found from stackOverFlow : https://stackoverflow.com/questions/27941661/generating-one-class-member-per-variadic-template-argument/53112843#53112843?newreg=f97a957ca4ca467bab0d0ce1cc6ea7b2
+// Technical inspired about understanding example found from stackOverFlow :
+// https://stackoverflow.com/questions/27941661/generating-one-class-member-per-variadic-template-argument/53112843#53112843?newreg=f97a957ca4ca467bab0d0ce1cc6ea7b2
 
-    //Variadic template class
-    template<class LType, class... RType> class ResourcesManager;
+// Variadic template class forward declaration
+template <class LType, class... RType>
+class ResourcesManager;
 
-    //Resources of one type. Will be created for each differente type.
-    template<class LType>
-    class ResourcesManager<LType>
-    {
-        public:
+/**
+ * @brief Resources of one type. Will be created for each differente type.
+ * @tparam LType 
+*/
+template <class LType>
+class ResourcesManager<LType>
+{
+protected:
+    std::unordered_map<std::string, LType> m_resources;
 
-        ResourcesManager ()                                = default;
-        ResourcesManager (const ResourcesManager& other)                = delete;
-        ResourcesManager&  operator= (const ResourcesManager& other)  = delete;
-        ResourcesManager (ResourcesManager&& other)       = default;
-        ~ResourcesManager ()                               = default;
+public:
+    ResourcesManager() noexcept = default;
+    ResourcesManager(const ResourcesManager& other) noexcept = delete;
+    ResourcesManager& operator=(const ResourcesManager& other) noexcept = delete;
+    ResourcesManager(ResourcesManager&& other) noexcept = default;
+    ResourcesManager& operator=(ResourcesManager&& other) noexcept = default;
+    ~ResourcesManager() noexcept = default;
 
-        protected:
+public:
+    /**
+     * @brief return element of index
+     *
+     * @param key
+     * @return LType&
+     */
+    LType* get(const std::string& key) noexcept;
 
-        /**
-         * @brief return element of index
-         * 
-         * @param key 
-         * @return LType& 
-         */
-        LType* get(const std::string& key)
-        {    
-            //iterator of LType
-            auto it = resource_.find(key);
-            if (it == resource_.end())
-            {
-                return nullptr; 
-            }
+    /**
+     * @brief add element with argument of constructor in parameter and key. Resource canno't have the same key
+     *
+     * @tparam Args
+     * @param args
+     * @return unsigned int
+     */
+    template <typename... Args>
+    LType& add(std::string key, Args&&... args) noexcept;
 
-            return &it->second;
-        }
+    /**
+     * @brief Remove resource with the corresponding key if key is found
+     * @param key 
+     * @return 
+    */
+    inline void remove(const std::string& key) noexcept;
 
-        /**
-         * @brief add element with argument of constructor in parameter
-         * 
-         * @tparam Args 
-         * @param args 
-         * @return unsigned int 
-         */
-        template<typename ...Args>
-        LType& add(std::string key, Args&&... args)
-        {
-            //auto for pair of iterator of LType and bool
-            auto rst = resource_.emplace(  std::piecewise_construct,
-                                            std::forward_as_tuple(key),
-                                            std::forward_as_tuple(args...));
+    /**
+     * @brief clear the container and all associate resources
+     * @return 
+    */
+    inline void clear() noexcept;
+};
 
-            if (rst.second == false)
-            {
-                Engine::Core::Debug::SLog::logError(std::string("resource insert with same key as an element existing : ") + key + ". Resource type : " + typeid(LType).name());
-                exit(1);
-            }
+/**
+ * @brief wrap multiple specifique resource manager.
+ * create a class hierarchy where each base class is a specialization for one of the types
+ * For example : resourceManager<int, float, std::string> wille create :
+ * resourceManager<int>, resourceManager<float> and resourceManager<std::string>.
+ * resourceManager<int, float, std::string> inherited about these 3 class and can access to each element
+ * @tparam LType 
+ * @tparam ...RType 
+*/
+template <class LType, class... RType>
+class ResourcesManager : public ResourcesManager<LType>, public ResourcesManager<RType...>
+{
+public:
+    ResourcesManager() noexcept = default;
+    ResourcesManager(const ResourcesManager& other) noexcept = delete;
+    ResourcesManager& operator=(const ResourcesManager& other) noexcept = delete;
+    ResourcesManager(ResourcesManager&& other) noexcept = default;
+    ResourcesManager& operator=(ResourcesManager&& other) noexcept = default;
+    ~ResourcesManager() noexcept = default;
 
-            return rst.first->second;
-        }
+    /**
+     * @brief Return resource.
+     * @example get<Texture>("AwsomeTexture")
+     * @tparam T
+     * @param key
+     * @return T&
+     */
+    template <class T>
+    inline T* get(const std::string& key) noexcept;
 
-        void remove(const std::string& key)
-        {
-            resource_.erase(key);
-        }
+    /**
+     * @brief Remove resource with the corresponding key if key is found
+     * @example remove<Texture>("AwsomeTexture")
+     * @tparam T 
+     * @param key 
+     * @return 
+    */
+    template <class T>
+    inline void remove(const std::string& key) noexcept;
 
-        void clearResource  () noexcept
-        {
-            resource_.clear();
-        }
+    /**
+     * @brief Add resource.
+     * @example add<Texture>("AwsomeTexture", Texture())
+     * @tparam T
+     * @tparam Args
+     * @param args
+     * @return unsigned int
+     */
+    template <class T, typename... Args>
+    inline T& add(const std::string& key, Args&&... args) noexcept;
 
-        private:
+    /**
+     * @brief clear the container and all associate resources
+     * @example clear<Texture>()
+     * @tparam T 
+     * @return 
+    */
+    template <class T>
+    inline void clear() noexcept;
+};
 
-        std::unordered_map <std::string, LType> resource_;
+} // namespace Engine::Resources
 
-    };
-
-    //Global resource manager.
-    //create a class hierarchy where each base class is a specialization for one of the types
-    //For example : resourceManager<int, float, std::string> wille create :
-    //resourceManager<int>, resourceManager<float> and resourceManager<std::string>.
-    //resourceManager<int, float, std::string> inherited about these 3 class and can access to each element
-    
-    template<class LType, class... RType>
-    class ResourcesManager
-        : public ResourcesManager<LType>, public ResourcesManager<RType...>
-    {
-        public:
-
-            #pragma region constructor/destructor
-
-            ResourcesManager ()                                = default;
-            ResourcesManager (const ResourcesManager& other)  = delete;
-            ResourcesManager (ResourcesManager&& other)       = default;
-            ~ResourcesManager ()                               = default;
-
-            #pragma endregion //!constructor/destructor
-
-            #pragma region methods
-            #pragma endregion //!methods
-
-            #pragma region accessor
-
-            /**
-             * @brief Return resource. Example : get<Texture>(0)
-             * 
-             * @tparam T 
-             * @param key 
-             * @return T& 
-             */
-            template<class T>
-            T* get(const std::string& key)
-            {
-                return ResourcesManager<T>::get(key);
-            }
-
-            #pragma endregion //!accessor
-
-            template<class T>
-            void remove(const std::string& key)
-            {
-                ResourcesManager<T>::remove(key);
-            }
-
-            #pragma region mutator
-
-            /**
-             * @brief Add resource. Example : add<Texture>(Texture())
-             * 
-             * @tparam T 
-             * @tparam Args 
-             * @param args 
-             * @return unsigned int 
-             */
-            template<class T, typename... Args>
-            T& add(const std::string& key, Args&&... args)
-            {
-                return ResourcesManager<T>::add(key, std::forward<Args>(args)...);
-            }
-
-            template<class T>
-            void clearContenorOf  () noexcept
-            {
-                ResourcesManager<T>::clearResource();
-            }
-
-            #pragma endregion //!mutator
-
-        protected:
-
-        private:
-
-    };
-
-}// namespaceEngine::Resources
+#include "ResourcesManager.inl"
---
+++ engine/src/ResourcesManager.inl
@@ -0,0 +1,72 @@
+#include "Engine/Resources/ResourcesManager.hpp"
+
+template <class LType>
+LType* Engine::Resources::ResourcesManager<LType>::get(const std::string& key) noexcept
+{
+    // iterator of LType
+    auto it = m_resources.find(key);
+    if (it == m_resources.end())
+    {
+        return nullptr;
+    }
+
+    return &it->second;
+}
+
+template <class LType>
+template <typename... Args>
+LType& Engine::Resources::ResourcesManager<LType>::add(std::string key, Args&&... args) noexcept
+{
+    // auto for pair of iterator of LType and bool
+    auto rst =
+        m_resources.emplace(std::piecewise_construct, std::forward_as_tuple(key), std::forward_as_tuple(args...));
+
+    if (rst.second == false)
+    {
+        Engine::Core::Debug::Log::logError(std::string("resource insert with same key as an element existing : ") +
+                                           key + ". Resource type : " + typeid(LType).name());
+        exit(1);
+    }
+
+    return rst.first->second;
+}
+
+template <class LType>
+void Engine::Resources::ResourcesManager<LType>::remove(const std::string& key) noexcept
+{
+    m_resources.erase(key);
+}
+
+template <class LType>
+void Engine::Resources::ResourcesManager<LType>::clear() noexcept
+{
+    m_resources.clear();
+}
+
+template <class LType, class... RType>
+template <class T>
+T* Engine::Resources::ResourcesManager<LType, RType...>::get(const std::string& key) noexcept
+{
+    return Engine::Resources::ResourcesManager<T>::get(key);
+}
+
+template <class LType, class... RType>
+template <class T>
+void Engine::Resources::ResourcesManager<LType, RType...>::remove(const std::string& key) noexcept
+{
+    Engine::Resources::ResourcesManager<T>::remove(key);
+}
+
+template <class LType, class... RType>
+template <class T, typename... Args>
+T& Engine::Resources::ResourcesManager<LType, RType...>::add(const std::string& key, Args&&... args) noexcept
+{
+    return Engine::Resources::ResourcesManager<T>::add(key, std::forward<Args>(args)...);
+}
+
+template <class LType, class... RType>
+template <class T>
+void Engine::Resources::ResourcesManager<LType, RType...>::clear() noexcept
+{
+    Engine::Resources::ResourcesManager<T>::clear();
+}
