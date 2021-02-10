6925a65baf3706309bff85d216383661ac0fc73e
Thomas DALLARD <t.dallard@student.isartdigital.com>
2021-02-10 11:54:32 +0100

    GameApiMacros.hpp added to define if using dllimport or dllexport

--- projects/GPGame/include/game.hpp
+++ projects/GPGame/include/game.hpp
@@ -1,13 +1,8 @@
 #pragma once
 
-#ifdef GAME_EXPORTS
-#define GAME_API __declspec(dllexport)
-#else
-#define GAME_API __declspec(dllimport)
-#endif
 
 #include "Engine/Core/Game/AbstractGame.hpp"
-#include <iostream>
+#include "GameApiMacros.hpp"
 
 class Game final : public AbstractGame
 {
