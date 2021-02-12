ffd46610496ee8044de662bff2f149ce4b20df64
Thomas DALLARD <t.dallard@student.isartdigital.com>
2021-02-12 10:12:38 +0100

    readme fixed and moved to root, gameStartup : std functions moved to constructor

--- launcher/gameStartup.cpp
+++ launcher/gameStartup.cpp
@@ -1,22 +1,20 @@
 #include "GameStartup.hpp"
-
 #include "Engine/Core/Game/AbstractGame.hpp"
 #include "Game.hpp"
-
 #include "Engine/Core/Debug/Assert.hpp"
 
 GameStartup::GameStartup() : m_game(createGameInstance())
 {
+	GPE_ASSERT(m_game != nullptr, "m_game should be valid since we're running the game.");
 
+	gameFunctionsPtr.update			= std::bind(&AbstractGame::update, m_game, std::placeholders::_1, std::placeholders::_2);
+	gameFunctionsPtr.fixedUpdate	= std::bind(&AbstractGame::fixedUpdate, m_game, std::placeholders::_1, std::placeholders::_2);
+	gameFunctionsPtr.render			= std::bind(&AbstractGame::render, m_game);
 }
 
 void GameStartup::update()
 {
-	GPE_ASSERT(m_game != nullptr, "m_game should be valid since we're running the game.");
-	std::function<void(double, double)> update = std::bind(&AbstractGame::update, m_game, std::placeholders::_1, std::placeholders::_2);
-	std::function<void(double, double)> fixedUpdate = std::bind(&AbstractGame::fixedUpdate, m_game, std::placeholders::_1, std::placeholders::_2);
-	std::function<void()> render = std::bind(&AbstractGame::render, m_game);
-	timeSystem.update(update, fixedUpdate, render);
+	timeSystem.update(gameFunctionsPtr.update, gameFunctionsPtr.fixedUpdate, gameFunctionsPtr.render);
 }
 
 GameStartup::~GameStartup()
