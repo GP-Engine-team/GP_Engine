46baaf213147c042681d1c2f1b16d76aed9c3e9d
s.amara <s.amara@student.isartdigital.com>
2021-03-10 13:43:57 +0100

    Fixed indentation

--- engine/include/Engine/Resources/Scene.hpp
+++ engine/include/Engine/Resources/Scene.hpp
@@ -10,42 +10,42 @@
 
 namespace GPE
 {
-    class Scene
+class Scene
+{
+    friend class SceneManager;
+
+private:
+    
+protected:
+
+    ResourceManagerType m_resourceManager;
+
+    void loadResource(ResourceManagerType& resourceManager) noexcept {};
+
+public:
+    inline Scene(ResourceManagerType& resourceManager) noexcept
     {
-        friend class SceneManager;
-
-        private:
+        loadResource(resourceManager);
+    }
     
-        protected:
+    //TODO: Can scene be created by default ?
+    inline
+    ~Scene () noexcept				                = delete;
 
-        ResourceManagerType m_resourceManager;
-
-        constexpr void loadResource(ResourceManagerType& resourceManager) noexcept {};
-
-        public:
-            constexpr inline Scene(ResourceManagerType& resourceManager) noexcept
-        {
-            loadResource(resourceManager);
-        }
+    //TODO: Can scene be copied ? How to manage resource
+    constexpr inline
+    Scene (const Scene& other) noexcept			    = delete;
     
-        //TODO: Can scene be created by default ?
-        inline
-        ~Scene () noexcept				                = delete;
-
-        //TODO: Can scene be copied ? How to manage resource
-        constexpr inline
-        Scene (const Scene& other) noexcept			    = delete;
-    
-        //TODO: Can scene be moved ? How to manage resource
-        constexpr inline
-        Scene (Scene&& other) noexcept				    = delete;
+    //TODO: Can scene be moved ? How to manage resource
+    constexpr inline
+    Scene (Scene&& other) noexcept				    = delete;
         
-        //TODO: Can scene be copied ? How to manage resource
-        constexpr inline
-        Scene& operator=(Scene const& other) noexcept		= delete;
+    //TODO: Can scene be copied ? How to manage resource
+    constexpr inline
+    Scene& operator=(Scene const& other) noexcept		= delete;
     
-        //TODO: Can scene be moved ? How to manage resource
-        constexpr inline
-        Scene& operator=(Scene && other) noexcept			= delete;    
-    };
+    //TODO: Can scene be moved ? How to manage resource
+    constexpr inline
+    Scene& operator=(Scene && other) noexcept			= delete;    
+};
 } /*namespace GPE*/
