f07b837ce67d148f5d9d26e8d4351305cfee5ee1
s.amara <s.amara@student.isartdigital.com>
2021-03-12 16:06:42 +0100

    Implemented the remaining needed methods

--- engine/include/Engine/Intermediate/Viewers/SceneViewer.hpp
+++ engine/include/Engine/Intermediate/Viewers/SceneViewer.hpp
@@ -14,19 +14,16 @@ class SceneViewer
 {
 public:
     Scene&        scene;
-    GameObject&   cameraOwner;
-    Camera        camera;
+    GameObject    cameraOwner;
 
     Texture       texture;
     RenderBuffer  depthBuffer;
     RenderBuffer  stencilBuffer;
-
-private:
-    GPE::RenderTexture::CreateArg generatedRenderTextureBuffers();
+    GLuint        framebufferID;
 
 public:
     SceneViewer(GPE::Scene& viewed, int width, int height);
-
+    ~SceneViewer();
     void resize(int width, int height);
 
     void render() const;
--- engine/src/SceneViewer.cpp
+++ engine/src/SceneViewer.cpp
@@ -6,23 +6,33 @@
 namespace GPE
 {
 
-GPE::RenderTexture::CreateArg SceneViewer::generatedRenderTextureBuffers()
-{
-    GPE::RenderTexture::CreateArg args;
-    args.colorBuffers.emplace_back(&texture);
-    args.depthBuffer   = &depthBuffer;
-    args.stencilBuffer = &stencilBuffer;
-    return args;
-}
-
-
 SceneViewer::SceneViewer(GPE::Scene& viewed, int width, int height)
-    : scene{viewed}, cameraOwner{scene.addGameObject(scene.getRoot())}, camera{cameraOwner, Camera::PerspectiveCreateArg()},
-      texture(Texture::CreateArg{width, height}),
+    : scene{viewed}, cameraOwner{scene, {"Editor_camera_0", {}, nullptr}},
+      texture({width, height}),
       depthBuffer({width, height}),
       stencilBuffer({width, height, RenderBuffer::EInternalFormat::STENCIL_INDEX8})
 {
+    const Camera::PerspectiveCreateArg args{16.f / 9.f, .001f, 1000.f, 90.f, "Editor_camera_0"};
+    cameraOwner.addComponent<Camera>(args);
 
+    glGenFramebuffers(1, &framebufferID);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebufferID);
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture.getID(), 0);
+    glDrawBuffer(GL_COLOR_ATTACHMENT0);
+
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuffer.getID());
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, stencilBuffer.getID());
+
+    GPE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Error in framebuffer generation");
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0u);
+}
+
+
+SceneViewer::~SceneViewer()
+{
+    glDeleteFramebuffers(1u, &framebufferID);
 }
 
 
@@ -32,6 +42,7 @@ void SceneViewer::resize(int width, int height)
     depthBuffer.resize(width, height);
     stencilBuffer.resize(width, height);
 }
+
 
 void SceneViewer::render() const
 {
