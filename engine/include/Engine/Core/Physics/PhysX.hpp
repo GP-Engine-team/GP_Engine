28e670db2657f05ad4625b19a4b2a620711aca88
NARDONE William <william.nardone@hotmail.fr>
2021-03-15 15:43:14 +0100

    +Add PhysX include files; +Start implementation;

---
+++ engine/include/Engine/Core/Physics/PhysX.hpp
@@ -0,0 +1,32 @@
+﻿/*
+ * Copyright (C) 2021 Amara Sami, Dallard Thomas, Nardone William, Six Jonathan
+ * This file is subject to the LGNU license terms in the LICENSE file
+ *	found in the top-level directory of this distribution.
+ */
+
+#pragma once
+#include "Engine/Core/Debug/Log.hpp"
+#include "PxFoundation.h"
+#include "foundation/PxErrorCallback.h"
+namespace GPE
+{
+class UserErrorCallback : public physx::PxErrorCallback
+{
+public:
+    virtual void reportError(physx::PxErrorCode::Enum code, const char* message, const char* file, int line)
+    {
+        FUNCT_ERROR(message);
+    }
+};
+
+class PhysX
+{
+public:
+    PhysX();
+    ~PhysX();
+
+private:
+    physx::PxFoundation* m_foundation;
+};
+
+} // namespace GPE
---
+++ engine/src/PhysX.cpp
@@ -0,0 +1,28 @@
+﻿#include "Engine/Core/Physics/PhysX.hpp"
+#include "PxPhysicsVersion.h"
+#include "extensions/PxDefaultAllocator.h"
+#include "extensions/PxDefaultErrorCallback.h"
+
+using namespace GPE;
+using namespace physx;
+
+static UserErrorCallback  gDefaultErrorCallback;
+static PxDefaultAllocator gDefaultAllocatorCallback;
+
+PhysX::PhysX()
+{
+
+    /* m_foundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback);
+     if (!m_foundation)
+         fatalError("PxCreateFoundation failed!");
+
+     bool recordMemoryAllocations = true;
+
+     mPvd                      = PxCreatePvd(*gFoundation);
+     PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+     mPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+     mPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation, PxTolerancesScale(), recordMemoryAllocations, mPvd);
+     if (!mPhysics)
+         fatalError("PxCreatePhysics failed!");*/
+}
