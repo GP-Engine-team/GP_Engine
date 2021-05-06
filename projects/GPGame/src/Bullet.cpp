94356bddc75db52ff85417b1e3d05a3771794fe9
s.amara <s.amara@student.isartdigital.com>
2021-05-06 20:08:08 +0200

    Moved method implementation from .hpp to .cpp

--- projects/GPGame/src/Bullet.cpp
+++ projects/GPGame/src/Bullet.cpp
@@ -1,3 +1,17 @@
 ﻿#include <Firearm/Bullet.hpp>
-
 File_GENERATED
+
+namespace GPG
+{
+
+Bullet::Bullet(float dammage) noexcept
+    : m_dammage{dammage}
+{
+}
+
+
+void Bullet::triggered()
+{
+}
+
+}
