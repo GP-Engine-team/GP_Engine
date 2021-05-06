67d64d9101bef2542f3af087a312caed4984a0a3
s.amara <s.amara@student.isartdigital.com>
2021-05-06 20:07:06 +0200

    Removed unused headers, and moved implementation to .cpp

--- projects/GPGame/include/Firearm/PPSH41.hpp
+++ projects/GPGame/include/Firearm/PPSH41.hpp
@@ -7,12 +7,9 @@
 #pragma once
 
 #include <Engine/Serialization/Inspect.hpp>
-#include <Engine/Serialization/DataInspector.hpp>
-#include "Firearm.hpp"
-
 #include <Engine/Serialization/Serialize.hpp>
-#include <Engine/Serialization/xml/xmlLoader.hpp>
-#include <Engine/Serialization/xml/xmlSaver.hpp>
+
+#include "Firearm.hpp"
 
 #include <Generated/PPSH41.rfk.h>
 
@@ -21,17 +18,15 @@ namespace GPG RFKNamespace()
     class RFKClass(ComponentGen, Serialize(), Inspect()) PPSH41 : public Firearm
     {
     public:
-        PPSH41(GPE::GameObject & owner) noexcept : Firearm(owner, GunMagazine(Bullet(5.f), 30, 30), 3.f, 0.1f)
-        {
-        }
+        PPSH41(GPE::GameObject & owner) noexcept;
 
-        PPSH41() noexcept                    = default;
+        PPSH41()                    noexcept = default;
         PPSH41(const PPSH41& other) noexcept = delete;
-        PPSH41(PPSH41 && other) noexcept     = delete;
-        virtual ~PPSH41() noexcept           = default;
+        PPSH41(PPSH41 && other)     noexcept = delete;
+        virtual ~PPSH41()           noexcept = default;
 
         PPSH41& operator=(PPSH41 const& other) noexcept = delete;
-        PPSH41& operator=(PPSH41&& other) noexcept = delete;
+        PPSH41& operator=(PPSH41&& other)      noexcept = delete;
 
         PPSH41_GENERATED
     };
