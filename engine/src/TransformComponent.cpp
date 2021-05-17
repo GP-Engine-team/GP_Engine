51e0b3269adb12f0bd509093ca68b12c28eadf23
s.amara <s.amara@student.isartdigital.com>
2021-05-17 13:25:43 +0200

    Fixed spacing

--- engine/src/TransformComponent.cpp
+++ engine/src/TransformComponent.cpp
@@ -10,7 +10,7 @@ namespace GPE
 {
 
 TransformComponent::TransformComponent(GameObject & refGameObject,
-                                        const TransformComponent::CreateArg& arg) noexcept
+                                       const TransformComponent::CreateArg& arg) noexcept
     : Component(refGameObject),
         m_spaceAttribut{GPM::toQuaternion(GPM::Transform::rotation(arg.eulerRotation)), arg.position, arg.scale},
         m_transform{GPM::toTransform(m_spaceAttribut)}
