dd3f71238044d1c75f1ee16116fbf241a39a6c35
s.amara <s.amara@student.isartdigital.com>
2021-05-25 08:51:25 +0200

    Added a method to render the gizmo itself

--- editor/include/Editor/SceneEditor.hpp
+++ editor/include/Editor/SceneEditor.hpp
@@ -1,4 +1,10 @@
-﻿#pragma once
+﻿/*
+ * Copyright (C) 2021 Amara Sami, Dallard Thomas, Nardone William, Six Jonathan
+ * This file is subject to the LGNU license terms in the LICENSE file
+ * found in the top-level directory of this distribution.
+ */
+
+#pragma once
 
 #include <Engine/Intermediate/Viewers/SceneViewer.hpp>
 #include <imgui/imguizmo.h>
@@ -25,22 +31,23 @@ public:
     GPE::SceneViewer view;
 
 private:
-    string_enum         m_operations[3];
-    string_enum         m_modes[2];
+    string_enum      m_operations[3];
+    string_enum      m_modes[2];
 
 public:
     ImGuizmo::OPERATION activeOperation;
     ImGuizmo::MODE      activeMode;
 
 private:
-    void         captureInputs      (bool toggle);
-    void         renderGizmoControls();
-    void         checkCursor        (GPE::IInspectable*& inspectedObject);
+    void         captureInputs        (bool toggle);
+    void         renderGizmoControlBar();
+    void         renderGizmo          (GPE::GameObject* inspected);
+    void         checkCursor          (GPE::GameObject*& inspectedObject);
 
 public:
     SceneEditor(GPE::Scene& scene);
 
-    void render(GPE::IInspectable*& inspectedObject);
+    void render(GPE::GameObject*& inspectedObject);
 };
 
 } // namespace Editor
