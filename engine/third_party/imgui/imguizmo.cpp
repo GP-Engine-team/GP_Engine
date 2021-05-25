8c9953b209b93d5ac7e543c59f4202d5294970a7
s.amara <s.amara@student.isartdigital.com>
2021-05-25 14:41:21 +0200

    Changed ImGuizmo's header name to match the filename

--- engine/third_party/imgui/imguizmo.cpp
+++ engine/third_party/imgui/imguizmo.cpp
@@ -21,11 +21,12 @@
 // SOFTWARE.
 
 #include "imgui.h"
+
 #ifndef IMGUI_DEFINE_MATH_OPERATORS
 #define IMGUI_DEFINE_MATH_OPERATORS
 #endif
 #include "imgui_internal.h"
-#include "ImGuizmo.h"
+#include "imguizmo.h"
 #if !defined(_WIN32)
 #define _malloca(x) alloca(x)
 #else
