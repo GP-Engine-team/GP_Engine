508edb8ac5c4d4199f732d005d9424f613b487a7
s.amara <s.amara@student.isartdigital.com>
2021-05-31 22:29:20 +0200

    Vertically aligned initialization

--- engine/src/TransformComponent.cpp
+++ engine/src/TransformComponent.cpp
@@ -13,7 +13,7 @@ using namespace GPM;
 TransformComponent::TransformComponent(GameObject& refGameObject, const TransformComponent::CreateArg& arg) noexcept
     : Component(refGameObject),
       m_spaceAttribut{GPM::toQuaternion(GPM::Transform::rotation(arg.eulerRotation)), arg.position, arg.scale},
-      m_transform{GPM::toTransform(m_spaceAttribut)}
+      m_transform    {GPM::toTransform(m_spaceAttribut)}
 {
     updateToSystem();
 }
