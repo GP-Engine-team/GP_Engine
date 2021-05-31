513f6b343c8b7b24a9b580e5bd477227f5ef9134
s.amara <s.amara@student.isartdigital.com>
2021-05-31 17:13:10 +0200

    Fixed newline spacing

--- engine/src/TransformComponent.cpp
+++ engine/src/TransformComponent.cpp
@@ -11,8 +11,8 @@ File_GENERATED
 using namespace GPM;
 
 TransformComponent::TransformComponent(GameObject& refGameObject, const TransformComponent::CreateArg& arg) noexcept
-    : Component(refGameObject), m_spaceAttribut{GPM::toQuaternion(GPM::Transform::rotation(arg.eulerRotation)),
-                                                arg.position, arg.scale},
+    : Component(refGameObject),
+      m_spaceAttribut{GPM::toQuaternion(GPM::Transform::rotation(arg.eulerRotation)), arg.position, arg.scale},
       m_transform{GPM::toTransform(m_spaceAttribut)}
 {
     updateToSystem();
